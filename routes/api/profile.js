58fc51c54dbfdaccbb360114cfcb4edfeac6f4d7
mcurci95 <mcurci95@gmail.com>
2020-05-01 21:56:16 -0700

    PUT endpoint to add experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -167,7 +167,42 @@ router.put('/experience', [auth, [
     
     
 ]], async (req, res) => {
-    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+        title, 
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const newExperience = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    };
+
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        profile.experince.unshift(newExperience);
+        await profile.save();
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error!");
+    }
 })
 
 module.exports = router;
