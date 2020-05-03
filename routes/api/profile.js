36af866b0b4f32d917431ec1e8608056f505d68c
mcurci95 <mcurci95@gmail.com>
2020-05-02 22:24:00 -0700

    Add education endpoint

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -222,6 +222,65 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
     } catch (error) {
         console.error(error);
     }
+});
+
+
+
+// @route   PUT api/profile/education
+// @desc    Add profile education
+// @access  private
+router.put('/education', [auth, [
+    check('school', 'School is required')
+    .not()
+    .isEmpty(),
+    check('degree', 'Degree is required')
+    .not()
+    .isEmpty(),
+    check('fieldOfStudy', 'Field of study is required')
+    .not()
+    .isEmpty(),
+    check('from', 'From date is required')
+    .not()
+    .isEmpty()
+    
+    
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+        school, 
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const newEducation = {
+        school, 
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    };
+
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        profile.education.unshift(newEducation);
+        await profile.save();
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error!");
+    }
 })
 
 module.exports = router;
