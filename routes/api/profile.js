adc7c653c3d345a32a09ba99691f5218a1bf15d4
Mcurci95 <mcurci95@gmail.com>
2020-05-09 22:44:50 -0700

    Fixing return issue

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -320,7 +320,7 @@ router.get('/github/:username', (req, res) => {
             if (error) console.error(error);
 
             if (response.statusCode !== 200) {
-                res.status(404).json({msg: "Github not found!"});
+                return res.status(404).json({msg: "Github not found!"});
             }
 
             res.json(JSON.parse(body));
